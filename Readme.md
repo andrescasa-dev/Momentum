260c9b1d88b560775ef91704dc38bff95430bccf
Andres <andrescasa.dev@gmail.com>
2022-09-20 17:24:29 -0500

    try catch in unsplash async funciton

--- Readme.md
+++ Readme.md
@@ -2,3 +2,6 @@
 + Backgrounds ([unesplash api](https://unsplash.com/documentation))
 + [weather](openWather api)
 + [cripto](coinweko api)
+
+# to analyze
+- [ ] trying using headers for credentials in unsplash api
--- src/events/getBackgroundImage.js
+++ src/events/getBackgroundImage.js
@@ -3,31 +3,24 @@ import { a_author } from "../utils/view.js";
 
 
 export default async function getBackgroundImage(){
-  const {urls, user} = await getImage();
-  document.body.style.backgroundImage = `url(${urls.full})`
-  a_author.textContent = user.username;
-  a_author.href = user.portfolio_url;
+  const URL_BACKGROUND_DEFAULT = "https://images.unsplash.com/photo-1491466424936-e304919aada7?crop=entropy&cs=tinysrgb&fm=jpg&ixid=MnwzNjU0MDR8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NjM3MTIwNjY&ixlib=rb-1.2.1&q=80"
+  try {
+    const {urls, user} = await getImage();
+    console.log(urls.full);
+    document.body.style.backgroundImage = `url(${urls.full})`
+    a_author.textContent = user.name;
+    a_author.href = user.portfolio_url;
+  } catch (error) {
+    document.body.style.backgroundImage = `url(${URL_BACKGROUND_DEFAULT})`
+    console.error("Error: " + error.message)
+  }
 }
 
 
 async function getImage(){
-  console.log(`${UNSPLASH_BASE_URL}/photos/random/?orientation=landscape&&client_id=${UNSPLASH_ACCESS_KEY}`)
-  const response = await fetch(`${UNSPLASH_BASE_URL}/photos/random/?orientation=landscape&&client_id=${UNSPLASH_ACCESS_KEY}`)
-  return await response.json();
+  debugger
+    const response = await fetch(`${UNSPLASH_BASE_URL}/Sphotos/random/?orientation=landscape&&query=night&&client_id=${UNSPLASH_ACCESS_KEY}`)
+    if(!response.ok) throw new Error('Incorrect URL')
+    return await response.json();
+
 }
-
-//======== trying using headers for credentials======//
-
-// async function getImage(){
-//   const response = await fetch(`${UNSPLASH_BASE_URL}/photos`,{
-//     method: 'GET',
-//     withCredentials: true,
-//     credentials: 'include',
-//     headers: {
-//       "Accept-Version": "v1",
-//       "Authorization" : UNSPLASH_ACCESS_KEY
-//     }
-//   });
-//   const data = await response.json();
-//   console.log()
-// }
